f33b06a3bf62d53c9a0801a80293d86fe692ba25
karreddy <111560009+karreddy@users.noreply.github.com>
2024-01-22 16:54:54 +0530

    Add files via upload

---
+++ testportlet/src/main/java/testportlet/constants/TestportletPortletKeys.java
@@ -0,0 +1,11 @@
+package testportlet.constants;
+
+/**
+ * @author USER
+ */
+public class TestportletPortletKeys {
+
+	public static final String TESTPORTLET =
+		"testportlet_TestportletPortlet";
+
+}
